1cf2ff4279507652de43a766a4b3923c9a0db46f
pchekushkin <pchekushkin@clink.tech>
2023-11-19 17:55:44 +0400

    feat(ui): добавил иконку в компонент

--- libs/frontend/ui/icon/src/lib/icon/icon.component.ts
+++ libs/frontend/ui/icon/src/lib/icon/icon.component.ts
@@ -1,12 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TuiButtonModule } from '@taiga-ui/core';
 
 @Component({
 	selector: 'oc-icon',
 	standalone: true,
-	imports: [CommonModule],
+	imports: [CommonModule, TuiButtonModule],
 	templateUrl: './icon.component.html',
 	styleUrls: ['./icon.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IconComponent {}
+export class IconComponent {
+	public onClick($event: MouseEvent): void {
+		console.log($event);
+	}
+}
