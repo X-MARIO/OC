4eb6f4d911980e8cefa80d24e30c3a6ab14c78fb
pchekushkin <pchekushkin@clink.tech>
2023-12-26 01:36:17 +0400

    feat(libs): мультирабочий стол удаление и обновление элемента

--- libs/frontend/matrix/api/src/lib/matrix-api.mock.service.ts
+++ libs/frontend/matrix/api/src/lib/matrix-api.mock.service.ts
@@ -90,10 +90,74 @@ export class MatrixApiMockService {
 	}
 
 	public updateOne(matrixEl: MatrixElement): Observable<MatrixElement> {
+
+		const authToken: IUserSecretsF['access_token'] = this.localSyncStorage.getItem(StorageKeys.AuthToken) ?? '';
+		const matrixSrt: string = this.localSyncStorage.getItem(StorageKeys.Matrix) ?? '';
+
+		const mapMatrix: Map<IUserCreate['username'], MatrixElement[][]> =
+			new Map<IUserCreate['username'], MatrixElement[][]>(
+				JSON.parse(matrixSrt),
+			);
+
+		const access: IUserCreate = jwtDecode<IUserCreate>(authToken);
+
+		const matrixArr: MatrixElement[][] = mapMatrix.get(access.username) ?? [];
+
+		const result: MatrixElement[][] = matrixArr.map((item: IMatrixElement[]) => {
+			return plainToInstance<MatrixElement, IMatrixElement>(MatrixElement, item);
+		})
+
+		const el: MatrixElement[] | undefined = result.find(
+			(value: MatrixElement[]) => {
+				return value[0]?.placeId === matrixEl.placeId;
+			},
+		);
+
+		if (!el) {
+			throw new Error('e89b92b9-241b-4c9c-bdc1-7603640156ce');
+		}
+
+		const matrixEl0: MatrixElement | undefined = el[0];
+
+		if (matrixEl0 === undefined) {
+			throw new Error('7ef7cb07-9c6a-4d66-b674-76c137401079');
+		}
+
+		matrixEl0.update(matrixEl);
+
+		mapMatrix.set(access.username, result);
+
+		this.localSyncStorage.setItem(StorageKeys.Matrix, JSON.stringify(Array.from(mapMatrix.entries())));
+
 		return of(matrixEl);
 	}
 
 	public deleteOne(placeId: MatrixElement['_placeId']): Observable<MatrixElement['_placeId']> {
+
+		const authToken: IUserSecretsF['access_token'] = this.localSyncStorage.getItem(StorageKeys.AuthToken) ?? '';
+		const matrixSrt: string = this.localSyncStorage.getItem(StorageKeys.Matrix) ?? '';
+
+		const mapMatrix: Map<IUserCreate['username'], MatrixElement[][]> =
+			new Map<IUserCreate['username'], MatrixElement[][]>(
+				JSON.parse(matrixSrt),
+			);
+
+		const access: IUserCreate = jwtDecode<IUserCreate>(authToken);
+
+		const matrixArr: MatrixElement[][] = mapMatrix.get(access.username) ?? [];
+
+		const result: MatrixElement[][] = matrixArr.map((item: IMatrixElement[]) => {
+			return plainToInstance<MatrixElement, IMatrixElement>(MatrixElement, item);
+		})
+
+		const newMatrix: MatrixElement[][] = result.filter(
+			(value: MatrixElement[]) => value[0]?.placeId !== placeId,
+		);
+
+		mapMatrix.set(access.username, newMatrix);
+
+		this.localSyncStorage.setItem(StorageKeys.Matrix, JSON.stringify(Array.from(mapMatrix.entries())));
+
 		return of(placeId);
 	}
 }
